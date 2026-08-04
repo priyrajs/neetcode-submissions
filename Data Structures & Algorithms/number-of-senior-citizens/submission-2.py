c94ad8d7c34c56d388c75b1b94ff220739cb1988
priyrajs <87078209+priyrajs@users.noreply.github.com>
2026-08-04 09:10:58 +0530

    Add: number-of-senior-citizens - submission-2

---
+++ Data Structures & Algorithms/number-of-senior-citizens/submission-2.py
@@ -0,0 +1,9 @@
+class Solution:
+    def countSeniors(self, details: List[str]) -> int:
+        op = 0
+        for i in details:
+            gender = 'M' if 'M' in i else 'F' if 'F' in i else 'O'
+            age = i.split(gender)[1][:2]
+            if int(age) > 60:
+                op += 1
+        return op
